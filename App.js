4836c2f1bbe7525a7085773a5b5648cbe4ce75c8
SB <sb@SARARATs-MacBook-Pro.local>
2021-04-01 20:19:21 -0400

    added side-links

--- App.js
+++ App.js
@@ -42,15 +42,25 @@ export default function App() {
         <ScrollView>
           <Text style={styles.bodyHead}>The Article Title</Text>
 
-          <Text style={styles.content}>You may use this template on any site, anywhere, for free just please leave the link back to me in the footer. This template validates XHTML Strict 1.0, CSS Validates as well; enjoy `:)`</Text> 
-          <Text style={styles.content}> 
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer mi. Vivamus sit amet neque vitae sapien bibendum sodales. Curabitur elementum. Duis imperdiet. Donec eleifend porttitor sapien. Praesent leo. Quisque auctor velit sed tellus. Suspendisse potenti. Aenean laoreet imperdiet nunc. Donec commodo suscipit dolor. Aenean nibh. Sed id odio. Aliquam lobortis risus ut felis. Sed vehicula pellentesque quam.</Text>
+          <View style={styles.topContent}>
+            <View style={styles.left}>
+              <Text style={styles.content}>You may use this template on any site, anywhere, for free just please leave the link back to me in the footer. This template validates XHTML Strict 1.0, CSS Validates as well; enjoy `:)`</Text> 
+              <Text style={styles.content}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer mi. Vivamus sit amet neque vitae sapien bibendum sodales. Curabitur elementum. Duis imperdiet. Donec eleifend porttitor sapien. Praesent leo. Quisque auctor velit sed tellus. Suspendisse potenti. Aenean laoreet imperdiet nunc. Donec commodo suscipit dolor. Aenean nibh. Sed id odio. Aliquam lobortis risus ut felis. Sed vehicula pellentesque quam.</Text>
+            </View>
+            <View style={styles.right}>
+              <Text>Links:</Text>
+              <Text>Web Design</Text>
+              <Text>Templates</Text>
+              <Text>Marketing</Text>
+              <Text>SEO</Text>
+              <Text>Programming</Text>
+              <Text>Consulting</Text>
+            </View>
+          </View>
 
-          <Text style={styles.content}>
-          Vestibulum augue quam, interdum id, congue semper, convallis non, velit. Quisque augue tortor, tristique ac, scelerisque eget, aliquam id, sem. Aenean lorem. Fusce velit nibh, dapibus quis, laoreet nec, porta a, dui. Nullam ac urna. Proin eget elit. Nunc scelerisque venenatis urna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce congue, turpis ut commodo mattis, pede erat fringilla tellus, pulvinar suscipit odio lorem sed pede.
-          </Text>
+            <Text style={styles.content}>Vestibulum augue quam, interdum id, congue semper, convallis non, velit. Quisque augue tortor, tristique ac, scelerisque eget, aliquam id, sem. Aenean lorem. Fusce velit nibh, dapibus quis, laoreet nec, porta a, dui. Nullam ac urna. Proin eget elit. Nunc scelerisque venenatis urna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce congue, turpis ut commodo mattis, pede erat fringilla tellus, pulvinar suscipit odio lorem sed pede.</Text>
         </ScrollView>
-        </View>
+      </View>
 
   </View>
 
@@ -132,9 +142,20 @@ const styles = ScaledSheet.create({
     fontSize: '15@s',
   },
 
+  topContent:{
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  left:{
+    width: '260@s'
+  },
+  right:{
+    width: '50@s'
+  },
   content:{
     fontFamily: 'Arial',
-    lineHeight: '10@s',
+    fontSize: '7@s',
+    lineHeight: '14@s',
     marginBottom: '15@s',
 
   }
